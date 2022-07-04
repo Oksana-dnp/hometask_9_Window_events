ee7e999dfc7c4fab0409ddd178f85fc33fdfee4a
Oksana Kharchenko <ksana.kharchenko@gmail.com>
2022-07-04 21:26:35 +0300

    only_first

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 let elemScrollDown = document.querySelector(".scroll-block__item--down");
 let elemScrollUp = document.querySelector(".scroll-block__item--up");
 
-let currentScrollY = window.screenY;
+//let currentScrollY = window.screenY;
+let prevScroll = window.screenY;
 window.addEventListener("scroll", changeSize);
-
-function changeSize() {
-    
-  if (window.scrollY > currentScrollY) {
+console.log(elemScrollDown);
+/* function changeSize() {
+  console.log('hi')
+  if (elemScrollDown.scrollY > currentScrollY) {
     currentScrollY = window.scrollY;
     if (!elemScrollDown.style.fontSize) {
       elemScrollDown.style.fontSize = `${
@@ -18,4 +19,21 @@ function changeSize() {
         parseInt(elemScrollDown.style.fontSize) + 2
       }px`;
   } 
+}
+ */
+
+function changeSize(e) {
+  let newScroll = window.scrollY;
+  if (!elemScrollDown.style.fontSize) {
+    let fontUp = `${parseInt(getComputedStyle(elemScrollDown).fontSize) + 4}px`;
+    newScroll > prevScroll ? (elemScrollDown.style.fontSize = fontUp) : (elemScrollDown.style.fontSize = fontDown);
+  } else {
+    let fontUp = `${parseInt(elemScrollDown.style.fontSize) + 4}`;
+    let fontDown = `${parseInt(elemScrollDown.style.fontSize) - 4}`;
+    if (newScroll > prevScroll && newScroll !== prevScroll && fontUp < 250)
+      elemScrollDown.style.fontSize = `${fontUp}px`;
+    if (newScroll < prevScroll && newScroll !== prevScroll && fontDown > 12)
+      elemScrollDown.style.fontSize = `${fontDown}px`;
+  }
+  prevScroll = window.scrollY;
 }
