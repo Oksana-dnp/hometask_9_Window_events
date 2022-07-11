97a48583549cf39b69db3b605c21f58026fc7d5a
Oksana Kharchenko <ksana.kharchenko@gmail.com>
2022-07-11 20:09:48 +0300

    110722

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let elemScrollUp = document.querySelector(".scroll-block__item--up");
 
 let prevScroll = window.screenY;
 let prevPos = window.scrollY;
+
 window.addEventListener("scroll", changeSize);
 window.addEventListener("scroll", moveBlocks);
 
@@ -26,9 +27,9 @@ function changeSize(e) {
 }
 
 
+
 function moveBlocks(e){
- /*  let blockLeft = document.querySelector('.scroll-block__content'); */
- let blockLeft =document.querySelector('.scroll-block__item-decor');
+  let blockLeft =document.querySelector('.scroll-block__item-decor');
   let blockRight = document.querySelector('.scroll-block__img');
   move(blockLeft, "left");
   move(blockRight, "right");
@@ -41,13 +42,47 @@ let stepDirection ;
 function move(elem,  direction) {
   let newPos = window.scrollY;
   let step;
-
-  if(window.scrollY > 250 && window.scrollY < 620){
+  let size;
+  if(window.scrollY > 500 && window.scrollY < 1000){
     if( (prevPos - newPos) < 0) stepDirection = 5;
-    else if((prevPos- newPos) > 0) stepDirection = -5;
-    direction == "left" ?  (step = parseInt(elem.style.width) + stepDirection ) : (step = parseInt(elem.style.right) + stepDirection );
+    else if((prevPos- newPos) > 0 ) stepDirection = -5;
+    direction == "left" ?  (step = parseInt(elem.style.width) + stepDirection) : (step = parseInt(elem.style.right) + stepDirection );
     direction == "left" ? (elem.style.width = step + "px") : (elem.style.right = step + "px");
-
+    if( direction == "left") {
+    size = parseInt(document.querySelector('.scroll-block__content').style.left);
+    console.log('size', size);
+    document.querySelector('.scroll-block__content').style.left = size + stepDirection + "px"
   } 
   prevPos = window.scrollY;
 }
+}
+
+document.querySelector('.scroll-up').onclick = function(e){
+  window.scrollTo(0,0);
+}
+document.querySelector('.scroll-up').onclick = e =>{
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  })
+}
+document.querySelector('.scroll-down').onclick = e =>{
+  window.scroll({
+    top: 700,
+    behavior: "smooth",
+  })
+}
+window.onscroll = ()=>{
+  console.log(window.scrollY)
+  let size;
+  if(window.scrollY > 990) {
+
+    if(document.querySelector('.scroll-up').style.fontSize){
+      console.log(document.querySelector('.scroll-up').style.fontSize);
+      size = parseInt(document.querySelector('.scroll-up').style.fontSize);
+    } else {
+      size = parseInt(getComputedStyle(document.querySelector('.scroll-up')).fontSize);
+    }
+     if(size > 12) document.querySelector('.scroll-up').style.fontSize =`${size - 1}px`;
+  }
+  }
